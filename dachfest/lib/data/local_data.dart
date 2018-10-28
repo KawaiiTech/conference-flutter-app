79cc91e60d5dd878fd2d5de7c3d33756c824439e
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2018-10-28 13:28:56 +0100

    load and parse data

--- dachfest/lib/data/local_data.dart
+++ dachfest/lib/data/local_data.dart
@@ -4,11 +4,50 @@ import 'package:dachfest/domain/domain.dart';
 import 'package:flutter/material.dart';
 
 Future<String> loadJson(BuildContext context) async {
-  return await DefaultAssetBundle.of(context).loadString('data_repo/dachfest-2018.json');
+  return await DefaultAssetBundle.of(context)
+      .loadString('data_repo/dachfest-2018.json');
 }
 
 Map<String, dynamic> decode(String string) {
   return parser.jsonDecode(string);
+}
+
+Schedule parseSchedule(Map<String, dynamic> data) {
+  final scheduleData = data['schedule'];
+  final day1Data = scheduleData['2018-11-10'];
+  final day2Data = scheduleData['2018-11-11'];
+  final day1 = parseDay(day1Data, data);
+  final day2 = parseDay(day2Data, data);
+  return Schedule(
+    day1: day1,
+    day2: day2,
+  );
+}
+
+Day parseDay(Map<String, dynamic> day1data, Map<String, dynamic> data) {
+  final List<dynamic> timeslots = day1data['timeslots'];
+
+  final track1 = <Talk>[];
+  final track2 = <Talk>[];
+  final track3 = <Talk>[];
+
+  for (final slot in timeslots) {
+    final List<dynamic> sessions = slot['sessions'];
+    final talk1 = sessions[0]['items'][0];
+    track1.add(Talk(title: talk1));
+    if (sessions.length > 1) {
+      final talk2 = sessions[1]['items'][0];
+      track2.add(Talk(title: talk2));
+    } else {
+      track2.add(Talk(title: ""));
+    }
+  }
+
+  return Day(
+    track1: Track(talks: track1),
+    track2: Track(talks: track2),
+    track3: Track(talks: track3),
+  );
 }
 
 List<Talk> parseTalks(Map<String, dynamic> data) {
--- dachfest/test/data/local_data_test.dart
+++ dachfest/test/data/local_data_test.dart
@@ -1,19 +1,39 @@
 import 'dart:io';
 
 import 'package:dachfest/data/local_data.dart';
+import 'package:dachfest/domain/domain.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Load JSON from file', () async {
-
-    final file = new File('data_repo/dachfest-2018.json');
-    final data = decode(await file.readAsString());
-    expect(data.isNotEmpty, true);
-
-    final talks = parseTalks(data);
-    expect(talks[0].title, "Registration");
-
-    print(talks);
+  test('Create Schedule and get first talk', () async {
+    Schedule schedule = await getSchedule();
+    expect(schedule.day1.track1.talks[0].title, "registration");
   });
 
+  test('Get second talk', () async {
+    Schedule schedule = await getSchedule();
+    expect(schedule.day1.track1.talks[1].title, "opening");
+  });
+
+  test('Track 2, slot 0, should be empty', () async {
+    Schedule schedule = await getSchedule();
+    expect(schedule.day1.track2.talks[0].title, "");
+  });
+
+  test('Track 2, slot 6, should be lightning-one', () async {
+    Schedule schedule = await getSchedule();
+    expect(schedule.day1.track2.talks[6].title, "lightning-one");
+  });
+
+  test('Last talk should be closing', () async {
+    Schedule schedule = await getSchedule();
+    expect(schedule.day2.track1.talks[11].title, "closing");
+  });
+}
+
+Future<Schedule> getSchedule() async {
+  final file = new File('data_repo/dachfest-2018.json');
+  final data = decode(await file.readAsString());
+  final schedule = parseSchedule(data);
+  return schedule;
 }
