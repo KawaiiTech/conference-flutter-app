1a50066418db7d39fe6a54449ed54f2bb9789f90
laramartin <lmartin@babbel.com>
2018-10-28 11:55:21 +0100

    Add TalkListRow widget, display mocked data

---
+++ dachfest/lib/presentation/talk_row.dart
@@ -0,0 +1,21 @@
+import 'package:dachfest/data/talk.dart';
+import 'package:flutter/material.dart';
+
+class TalkListRow extends StatelessWidget {
+  final Talk talk;
+
+  TalkListRow(
+    this.talk,
+  ) : assert(talk != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(talk.time),
+        Text(talk.title),
+        Text(talk.author),
+      ],
+    );
+  }
+}
--- dachfest/lib/presentation/talks_screen.dart
+++ dachfest/lib/presentation/talks_screen.dart
@@ -1,13 +1,43 @@
+import 'package:dachfest/data/talk.dart';
+import 'package:dachfest/presentation/talk_row.dart';
 import 'package:flutter/material.dart';
 
 class TalksScreen extends StatelessWidget {
+  final List<Talk> talks = [
+    Talk(
+        time: "10:00",
+        title: "Sketchnoting Workshop",
+        author: "Miquel and Lara"),
+    Talk(
+        time: "10:00",
+        title: "Sketchnoting Workshop",
+        author: "Miquel and Lara"),
+    Talk(
+        time: "10:00",
+        title: "Sketchnoting Workshop",
+        author: "Miquel and Lara"),
+    Talk(
+        time: "10:00",
+        title: "Sketchnoting Workshop",
+        author: "Miquel and Lara"),
+    Talk(
+        time: "10:00",
+        title: "Sketchnoting Workshop",
+        author: "Miquel and Lara"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("TalksScreen"),
       ),
-      body: Text("TODO: add talks list"),
+      body: ListView.builder(
+        itemCount: talks.length,
+        itemBuilder: (BuildContext context, int index) => TalkListRow(
+              talks[index],
+            ),
+      ),
     );
   }
 }
