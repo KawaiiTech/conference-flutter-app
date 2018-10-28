02972ab472375b5c3b953a680d9121a24ca586cd
laramartin <laramartin@users.noreply.github.com>
2018-10-28 20:17:58 +0100

    Add scrollable app bar to TalkDetailsScreen

--- dachfest/lib/presentation/talk_details_screen.dart
+++ dachfest/lib/presentation/talk_details_screen.dart
@@ -2,16 +2,46 @@ import 'package:dachfest/domain/domain.dart';
 import 'package:flutter/material.dart';
 
 class TalkDetailsScreen extends StatelessWidget {
-
   final Talk talk;
 
-  TalkDetailsScreen({this.talk}) : assert (talk != null);
+  TalkDetailsScreen({this.talk}) : assert(talk != null);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(talk.title),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              title: Text(
+                talk.title,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            expandedHeight: 150.0,
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16.0),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate([
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    talk.title,
+                    style: TextStyle(fontSize: 20.0),
+
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(talk.description),
+                ),
+              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
