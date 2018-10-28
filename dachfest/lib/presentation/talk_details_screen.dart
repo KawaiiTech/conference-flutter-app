4fea5b23a40d6c69768a87aa08b608ca13339620
laramartin <laramartin@users.noreply.github.com>
2018-10-28 21:04:36 +0100

    Add list of speakers in TalkDetailsScreen

--- dachfest/lib/presentation/talk_details_screen.dart
+++ dachfest/lib/presentation/talk_details_screen.dart
@@ -27,22 +27,49 @@ class TalkDetailsScreen extends StatelessWidget {
             sliver: SliverList(
               delegate: SliverChildListDelegate([
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(bottom: 8.0),
                   child: Text(
                     talk.title,
                     style: TextStyle(fontSize: 20.0),
-
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(bottom: 8.0),
                   child: Text(talk.description),
                 ),
+                buildSpeakersList(),
               ]),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildSpeakersList() {
+    if (talk.speakers.length == 0) {
+      return Container();
+    }
+    List<Widget> widgets = [
+      Padding(
+        padding: const EdgeInsets.only(
+          top: 16.0,
+          bottom: 8.0,
+        ),
+        child: Text(
+          "Speakers",
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ),
+    ];
+    for (var i = 0; i < talk.speakers.length; i++) {
+      widgets.add(
+        Text(talk.speakers[i].name),
+      );
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: widgets,
     );
   }
 }
